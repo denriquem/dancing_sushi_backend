f773b56fe21523b70a11e123f7c624ffa28a1fbd
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2023-04-11 11:40:47 +0100

    flask environemnt and docker setup

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+import uuid
+from flask import Flask, request
+from db import comments, reactions, images
 
 app = Flask(__name__)
 
-test = ['bla', 'bla2', 'bla3']
+
+@app.get("/images")
+def get_images():
+    return images
 
 
-@app.get("/store")
-def find_test():
-    return test
+@app.get("/images/<string:image_id>")
+def get_image_by_id(image_id):
+    try:
+        return images[image_id]
+    except KeyError:
+        return {"message": "store not found"}, 404
+
+
+@app.post("/image")
+def create_image():
+    request_data = request.get_json()
+    image_id = uuid.uuid4.hex()
+    newImage = {**request_data, id: image_id}
+    images[image_id] = newImage
+    return newImage, 201
+
+
+@app.get("/comments/<string:image_id>")
+def get_comments_for_image(image_id):
+    filtered_comments = []
+    for comment in comments:
+        if comment["image_id"] == image_id:
+            filtered_comments.append(comment)
+    return filtered_comments
---
+++ db.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+comments = {
+    1: {
+        "image_id": 9999,
+        "user_id": 202920920,
+        "commentBody": "I think this photo is wonderful, possibly one of my favoruites",
+        "created_at": datetime.now()
+    },
+    2: {
+        "image_id": 9999,
+        "user_id": 202920920,
+        "commentBody": "I think this photo is terrible",
+        "created_at": datetime.now()
+    }
+}
+
+reactions = {
+    1: {
+        "comment_id": 1,
+        "user_id": 9202920292,
+        "type": 'like'
+    }
+}
+
+images = {
+    1: {
+        "user_id": 9292929292,
+        "image_url": 'test.com'
+    }
+}
